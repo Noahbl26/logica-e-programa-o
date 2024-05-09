91dd2faacc53d245a1b93c31e705bc8741769e8a
Noah <163437246+Noahbl26@users.noreply.github.com>
2024-05-09 11:40:49 -0300

    identifica se numero e positivo ou negativo pyton

---
+++ main (6).py
@@ -0,0 +1,12 @@
+'''
+
+verifica o número é negativo,positivo ou zero
+
+'''
+nub=int(input("insira o número:"))
+if nub > 0:
+    print("seu número é positivo.")
+elif nub < 0:
+    print("seu nu´mero é negativo")
+else:
+    print("seu nu´mero é zero")
